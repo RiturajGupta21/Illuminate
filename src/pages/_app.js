798335126dae91bf879eb176e347ffc70329bd12
Rituraj Gupta <rituraj.iter@gmail.com>
2022-05-29 20:32:34 +0530

    Uncommented Script tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,10 +5,10 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head> 
+      <Head>
         <title>Illuminate -By CODEX</title>
       </Head>
-      {/* <Script src='./analytics.js' strategy="lazyOnload"></Script> */}
+      <Script src='./analytics.js' strategy="lazyOnload"></Script>
       <Theme>
         <Component {...pageProps} />
       </Theme>
